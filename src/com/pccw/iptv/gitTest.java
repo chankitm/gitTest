183fa20af7434660fd685a6930ec71715c0224ae
ArRoy <arroy@gmail.com>
2011-08-11 23:38:39 +0800

    A
    Signed-off-by: ArRoy <arroy@gmail.com>

---
+++ src/com/pccw/iptv/gitTest.java
@@ -0,0 +1,13 @@
+package com.pccw.iptv;
+
+public class gitTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello git!");
+	}
+
+}
